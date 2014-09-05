a71317ec0765759c03efa9d7852a60592a4a3088
codenotfound <dmitry.velikij@gmail.com>
2014-09-05 19:25:00 +0400

    started writing selectors for spider

--- scraping/metacriticbot/metacriticbot/spiders/metacritic.py
+++ scraping/metacriticbot/metacriticbot/spiders/metacritic.py
@@ -8,26 +8,14 @@ class MetacriticSpider(Spider):
     name = "metacritic"
     allowed_domains = ["metacritic.com"]
     start_urls = [
-        "http://www.metacritic.com/game",
-    ]
+            "http://www.metacritic.com/game/pc/b-17-flying-fortress-the-mighty-8th"
+            #        "http://www.metacritic.com/game",
+            ]
+
 
     def parse(self, response):
-        """
-        The lines below is a spider contract. For more info see:
-        http://doc.scrapy.org/en/latest/topics/contracts.html
+        filename = response.url.split("/")[-2]
+        with open(filename, 'wb') as f:
+            f.write(response.body)
 
-        ...
-
-        """
-        sel = Selector(response)
-        sites = sel.xpath('//ul[@class="directory-url"]/li')
-        items = []
-
-        for site in sites:
-            item = Website()
-            item['name'] = site.xpath('a/text()').extract()
-            item['url'] = site.xpath('a/@href').extract()
-            item['description'] = site.xpath('text()').re('-\s([^\n]*?)\\n')
-            items.append(item)
-
-        return items
+#TITLE - response.xpath('//div[@class="product_title"]/a/span[@itemprop="name"]/text()').extract()[0].strip()
